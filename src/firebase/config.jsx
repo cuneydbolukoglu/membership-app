ca472569465580e55f7fa7bd3209d7b337d364ca
cuneydbolukoglu <cuneydbolukogluu@gmail.com>
2021-01-04 00:27:22 +0300

    firebase için components edits

---
+++ src/firebase/config.jsx
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBWUzsX6lffkC0WqYJcfAOmAW2kryy7jK0",
+    authDomain: "membership-app-91af9.firebaseapp.com",
+    databaseURL: "https://membership-app-91af9-default-rtdb.firebaseio.com",
+    projectId: "membership-app-91af9",
+    storageBucket: "membership-app-91af9.appspot.com",
+    messagingSenderId: "425207285927",
+    appId: "1:425207285927:web:ab1e8cfc9bb1ebfbeff382"
+  };
+
+export default firebaseConfig;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,36 +1,25 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { setData } from '../components/data-controller';
-import { uuidv4 } from '../components/helper';
 import ErrorMessage from '../components/error-message';
-import { MATCH_PASWORD, NULL_PASSWORD, NULL_USERNAME } from '../components/message/message';
 import passwordHash from 'password-hash';
+import { auth } from '../firebase';
 
 const Register = props => {
-    const [username, setUsername] = useState(null);
+    const [email, setEmail] = useState(null);
     const [password, setPassword] = useState("");
-    const [repeatPassword, setRepeatPassword] = useState(null);
     const [errorMessage, setErrorMessage] = useState(null);
     const [errorResult, setErrorResult] = useState(null);
-
-    const newUser = { id: uuidv4(), username: username, password: passwordHash.generate(password) };
 
     const onButtonClick = e => {
         e.preventDefault();
 
-        if (username && password && password === repeatPassword) {
-            setData(newUser, function (response) {
-                console.log(response);
-                setErrorMessage(response.message);
-                setErrorResult(response.result);
+        auth.createUserWithEmailAndPassword(email, passwordHash.generate(password))
+            .then(res => {
+                console.log(res)
             })
-        } else if (!username) {
-            setErrorMessage(NULL_USERNAME);
-        } else if (!password) {
-            setErrorMessage(NULL_PASSWORD);
-        } else if (password !== repeatPassword) {
-            setErrorMessage(MATCH_PASWORD);
-        }
+            .catch(err => {
+                console.error(err)
+            })
     }
 
     return (
@@ -40,16 +29,12 @@ const Register = props => {
                 <form onSubmit={onButtonClick}>
                     <h1>New Account</h1>
                     <input type="text"
-                        placeholder="Username"
-                        onChange={(e) => { setUsername(e.target.value) }}
+                        placeholder="Email"
+                        onChange={(e) => { setEmail(e.target.value) }}
                     />
                     <input type="password"
                         placeholder="Password"
                         onChange={(e) => { setPassword(e.target.value) }}
-                    />
-                    <input type="password"
-                        placeholder="Password Match"
-                        onChange={(e) => { setRepeatPassword(e.target.value) }}
                     />
                     <button
                         onClick={onButtonClick}
