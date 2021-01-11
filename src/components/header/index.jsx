8f3ebc2dd41b5cf97a52ce82dfe781d7d90dd27b
cuneydbolukoglu <cuneydbolukogluu@gmail.com>
2021-01-11 21:44:53 +0300

    Header component match location

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,17 @@
-import logo from '../../assets/img/users-solid.svg'
+import { useLocation } from 'react-router-dom';
+import logo from '../../assets/img/users-solid.svg';
 
 const Index = props => {
 
-    const match = props.match
+    const location = useLocation();
+    const match = location.pathname === '/home'
 
     return (
         match ? (
             <header>
                 <div className="wrapper">
                     <div className="logo">
-                       <img src={logo} alt="logo"/>Membership App
+                        <img src={logo} alt="logo" />Membership App
                     </div>
                 </div>
             </header>
